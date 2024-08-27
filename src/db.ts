06a6bd2bc55516bb83c26b54cf52cd07bf2749fa
Ignatus2767 <127020105+Ignatus2767@users.noreply.github.com>
2024-08-27 22:13:32 +0000

    Update db.ts

--- src/db.ts
+++ src/db.ts
@@ -8,6 +8,7 @@ const signupDbPool = createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME_SIGNUP,
+  port: process.env.DB_PORT
 });
 
 const reviewDbPool = createPool({
