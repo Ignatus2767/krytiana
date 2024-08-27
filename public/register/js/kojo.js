4395de363475c9a92cea6a6e3f3c462a3ed26aaf
Ignatus2767 <ignatusdonkoh9@gmail.com>
2024-08-27 01:07:52 -1200

    0.5

--- public/register/js/kojo.js
+++ public/register/js/kojo.js
@@ -1,16 +1,21 @@
 document.addEventListener('DOMContentLoaded', function() {
+    const showFeedback = (message, type) => {
+        const feedbackMessage = document.getElementById('feedback-message');
+        feedbackMessage.textContent = message;
+        feedbackMessage.className = type; // 'success' or 'error'
+        feedbackMessage.style.display = 'block';
+        setTimeout(() => {
+            feedbackMessage.style.display = 'none';
+        }, 5000);
+    };
+
     const signupBtn = document.getElementById('signup-btn');
     const signinBtn = document.getElementById('signin-btn');
-   
-    const signupForm = document.getElementById('signup-form');
-    const signinForm = document.getElementById('signin-form');
-    const reviewForm = document.getElementById('review-form');
-    const reviewsList = document.getElementById('reviews-list');
-    const feedbackMessage = document.getElementById('feedback-message'); // Feedback message element
+    const signupFormElement = document.getElementById('signupForm');
+    const signinFormElement = document.getElementById('signinForm');
     const forgotPasswordLink = document.getElementById('forgot-password-link');
     const forgotPasswordForm = document.getElementById('forgot-password-form');
     const forgotPasswordFormElement = document.getElementById('forgotPasswordForm');
-    const forgotPasswordFeedback = document.getElementById('forgot-password-feedback');
 
     // Switch between forms
     signupBtn.addEventListener('click', function() {
@@ -21,27 +26,19 @@ document.addEventListener('DOMContentLoaded', function() {
         toggleActiveForm('signin');
     });
 
-   
-
     function toggleActiveForm(activeForm) {
-        // Reset all buttons
         signupBtn.classList.remove('active');
         signinBtn.classList.remove('active');
-       
 
-        // Hide all forms
-        signupForm.classList.add('hidden');
-        signinForm.classList.add('hidden');
-       
-
-        // Show the active form and button
         if (activeForm === 'signup') {
             signupBtn.classList.add('active');
-            signupForm.classList.remove('hidden');
+            signupFormElement.classList.remove('hidden');
+            signinFormElement.classList.add('hidden');
         } else if (activeForm === 'signin') {
             signinBtn.classList.add('active');
-            signinForm.classList.remove('hidden');
-        } 
+            signinFormElement.classList.remove('hidden');
+            signupFormElement.classList.add('hidden');
+        }
     }
 
     forgotPasswordLink.addEventListener('click', function(event) {
@@ -49,19 +46,15 @@ document.addEventListener('DOMContentLoaded', function() {
         forgotPasswordForm.classList.toggle('hidden');
     });
 
-    // Handle form submission for signup
-    const signupFormElement = document.getElementById('signupForm');
     signupFormElement.addEventListener('submit', function(event) {
-        event.preventDefault(); // Prevent default form submission
+        event.preventDefault();
 
-        const formData = new FormData(signupFormElement); // Get form data
+        const formData = new FormData(signupFormElement);
         const password = formData.get('password');
         const confirmPassword = formData.get('confirm-password');
 
-        // Validate password and confirm password
         if (password !== confirmPassword) {
             showFeedback('Passwords do not match. Please re-enter.', 'error');
-            console.log('Passwords do not match:', password, confirmPassword);
             return;
         }
 
@@ -76,9 +69,9 @@ document.addEventListener('DOMContentLoaded', function() {
         .then(data => {
             if (data.success) {
                 showFeedback('Signup successful!', 'success');
-                window.location.href = 'index.html'; // Redirect to signin page
+                window.location.href = 'index.html';
             } else {
-                showFeedback(data.message, 'error'); // Show specific error message
+                showFeedback(data.message, 'error');
             }
         })
         .catch(error => {
@@ -87,10 +80,10 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     });
 
-    // Handle Signin Form Submission
-    document.getElementById('signinForm').addEventListener('submit', function(event) {
+    signinFormElement.addEventListener('submit', function(event) {
         event.preventDefault();
-        const formData = new FormData(event.target);
+        const formData = new FormData(signinFormElement);
+
         fetch('/api/users/signin', {
             method: 'POST',
             headers: {
@@ -113,63 +106,10 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     });
 
-    // Handle Review Form Submission
-    document.getElementById('reviewForm').addEventListener('submit', function(event) {
-        event.preventDefault();
-        const formData = new FormData(event.target);
-        fetch('/api/reviews/add', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(Object.fromEntries(formData.entries()))
-        })
-        .then(response => response.json())
-        .then(data => {
-            if (data.success) {
-                showFeedback('Review submitted successfully!', 'success');
-                event.target.reset();
-                fetchReviews();
-            } else {
-                showFeedback(data.message, 'error');
-            }
-        })
-        .catch(error => {
-            console.error('Error:', error);
-            showFeedback('An error occurred. Please try again.', 'error');
-        });
-    });
-
-    // Function to fetch reviews from the server
-    const fetchReviews = () => {
-        fetch('/api/reviews/all')
-        .then(response => response.json())
-        .then(data => {
-            reviewsList.innerHTML = '';
-            data.forEach(review => {
-                const li = document.createElement('li');
-                li.textContent = `${review.username} (${new Date(review.date).toLocaleString()}): ${review.comment} - Rating: ${review.rating}`;
-                reviewsList.appendChild(li);
-            });
-        });
-    };
-
-    // Function to display feedback messages
-    const showFeedback = (message, type) => {
-        feedbackMessage.textContent = message;
-        feedbackMessage.className = `feedback-message ${type}`; // 'success' or 'error'
-        feedbackMessage.style.display = 'block';
-        setTimeout(() => {
-            feedbackMessage.style.display = 'none';
-        }, 5000);
-    };
-
-    // Initial fetch of reviews when the page loads
-    fetchReviews();
-
     forgotPasswordFormElement.addEventListener('submit', function(event) {
         event.preventDefault();
         const formData = new FormData(forgotPasswordFormElement);
+
         fetch('/api/users/forgot-password', {
             method: 'POST',
             headers: {
